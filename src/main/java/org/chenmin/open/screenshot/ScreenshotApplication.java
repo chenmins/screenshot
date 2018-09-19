be5b378c980cddb7eac790a6bf374d5404be1a23
chenmin <admin@chenmin.org>
2018-09-19 16:25:21 +0800

    Initial commit

---
+++ src/main/java/org/chenmin/open/screenshot/ScreenshotApplication.java
@@ -0,0 +1,12 @@
+package org.chenmin.open.screenshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenshotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenshotApplication.class, args);
+    }
+}
